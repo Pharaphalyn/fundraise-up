f842b5d180b1b85aa1a75a23e6e602e1eefb110e
Tudrug <108qwl@gmail.com>
2023-06-05 16:51:40 +0700

    Apply prettier formatting

--- app.ts
+++ app.ts
@@ -1,44 +1,44 @@
-import { User } from './interfaces/user';
-import 'dotenv/config';
-import { Collection, MongoClient, ObjectId } from 'mongodb';
-import { faker } from '@faker-js/faker';
+import { User } from "./interfaces/user";
+import "dotenv/config";
+import { Collection, MongoClient, ObjectId } from "mongodb";
+import { faker } from "@faker-js/faker";
 
 function createRandomUser(): User {
-    return {
-        _id: new ObjectId(),
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        email: faker.internet.email(),
-        createdAt: new Date(),
-        address: {
-            country: faker.location.countryCode(),
-            state: faker.location.state(),
-            city: faker.location.city(),
-            postcode: faker.location.zipCode(),
-            line1: faker.location.streetAddress(),
-            line2: faker.location.secondaryAddress()
-        }
-    };
+  return {
+    _id: new ObjectId(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+    createdAt: new Date(),
+    address: {
+      country: faker.location.countryCode(),
+      state: faker.location.state(),
+      city: faker.location.city(),
+      postcode: faker.location.zipCode(),
+      line1: faker.location.streetAddress(),
+      line2: faker.location.secondaryAddress(),
+    },
+  };
 }
 
 async function insertRandomUsers(collection: Collection) {
-    const users: User[] = faker.helpers.multiple(createRandomUser,
-        {count: Math.floor(Math.random() * 9) + 1});
-        collection.insertMany(users);
-    setTimeout(() => insertRandomUsers(collection), 200);
+  const users: User[] = faker.helpers.multiple(createRandomUser, {
+    count: Math.floor(Math.random() * 9) + 1,
+  });
+  collection.insertMany(users);
+  setTimeout(() => insertRandomUsers(collection), 200);
 }
 
 async function main() {
-    const client = new MongoClient(process.env.DB_URI);
+  const client = new MongoClient(process.env.DB_URI);
 
-    try {
-        await client.connect();
-        const customers = client.db().collection('customers');
-        await insertRandomUsers(customers);
-    }
-    catch(e) {
-        console.log(e);
-    }
+  try {
+    await client.connect();
+    const customers = client.db().collection("customers");
+    await insertRandomUsers(customers);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 main();
--- sync.ts
+++ sync.ts
@@ -1,98 +1,113 @@
-import { User } from './interfaces/user';
-import 'dotenv/config';
-import { ChangeStreamOptions, Collection, MongoClient } from 'mongodb';
-import { createHash } from 'crypto';
-import 'fs';
-import { writeFile } from 'fs';
-import { readFile } from 'fs/promises';
+import { User } from "./interfaces/user";
+import "dotenv/config";
+import { ChangeStreamOptions, Collection, MongoClient } from "mongodb";
+import { createHash } from "crypto";
+import "fs";
+import { writeFile } from "fs";
+import { readFile } from "fs/promises";
 
 let queries = [];
 let timeout;
 let resumeToken: Object;
 
 //Easy way to add or remove fields to encode
-const encodedFields = ['firstName', 'lastName', 'email',
-    'address.postcode', 'address.line1', 'address.line2'];
+const encodedFields = [
+  "firstName",
+  "lastName",
+  "email",
+  "address.postcode",
+  "address.line1",
+  "address.line2",
+];
 
-async function fullSync(customers: Collection, customersAnonymised: Collection) {
-    const allCustomers = await customers.find().toArray();
+async function fullSync(
+  customers: Collection,
+  customersAnonymised: Collection
+) {
+  const allCustomers = await customers.find().toArray();
 
-    const updateQuery = allCustomers.map(customer => {
-        return {
-            updateOne: {
-                filter: {_id: customer._id},
-                update: {$set: encodeCustomer(customer)},
-                upsert: true
-            }
-        }
-    });
-    await customersAnonymised.bulkWrite(updateQuery);
-    process.exit();
+  const updateQuery = allCustomers.map((customer) => {
+    return {
+      updateOne: {
+        filter: { _id: customer._id },
+        update: { $set: encodeCustomer(customer) },
+        upsert: true,
+      },
+    };
+  });
+  await customersAnonymised.bulkWrite(updateQuery);
+  process.exit();
 }
 
-async function continuousSync(customers: Collection, customersAnonymised: Collection) {
-    timeout = setTimeout(() => writeDocuments(customersAnonymised), 1000);
-    try {
-        const tokenFile = await readFile('resume_token.json');
-        if (tokenFile && tokenFile.toString()) {
-            resumeToken = JSON.parse(tokenFile.toString());
-        }
+async function continuousSync(
+  customers: Collection,
+  customersAnonymised: Collection
+) {
+  timeout = setTimeout(() => writeDocuments(customersAnonymised), 1000);
+  try {
+    const tokenFile = await readFile("resume_token.json");
+    if (tokenFile && tokenFile.toString()) {
+      resumeToken = JSON.parse(tokenFile.toString());
     }
-    catch (e) {
-        console.log(e);
+  } catch (e) {
+    console.log(e);
+  }
+  const options: ChangeStreamOptions = { fullDocument: "updateLookup" };
+  if (resumeToken) {
+    options.resumeAfter = resumeToken;
+  }
+  const changeStream = customers.watch([], options);
+  for await (const change of changeStream) {
+    if (
+      change.operationType === "update" ||
+      change.operationType === "insert"
+    ) {
+      const doc = change["fullDocument"];
+      queries.push({
+        updateOne: {
+          filter: { _id: change["documentKey"]._id },
+          update: { $set: encodeCustomer(doc) },
+          upsert: true,
+        },
+      });
+      resumeToken = change._id;
+      if (queries.length >= 1000) {
+        writeDocuments(customersAnonymised);
+      }
     }
-    const options: ChangeStreamOptions = { fullDocument: 'updateLookup' };
-    if (resumeToken) {
-        options.resumeAfter = resumeToken;
-    }
-    const changeStream = customers.watch([], options);
-    for await (const change of changeStream) {
-        if (change.operationType === 'update' || change.operationType === 'insert') {
-            const doc = change['fullDocument'];
-            queries.push({
-                updateOne: {
-                    filter: {_id: change['documentKey']._id},
-                    update: {$set: encodeCustomer(doc)},
-                    upsert: true
-                }
-            });
-            resumeToken = change._id;
-            if (queries.length >= 1000) {
-                writeDocuments(customersAnonymised);
-            }
-        }
-    }
+  }
 }
 
 function writeDocuments(collection: Collection) {
-    clearTimeout(timeout);
-    if (queries.length) {
-        collection.bulkWrite(queries);
-        console.log('Writing ' + queries.length + ' documents.');
-        queries = [];
-        writeFile('resume_token.json', JSON.stringify(resumeToken), err => {
-            if (err) {
-              console.error(err);
-            }
-        });
-    }
-    timeout = setTimeout(() => writeDocuments(collection), 1000);
+  clearTimeout(timeout);
+  if (queries.length) {
+    collection.bulkWrite(queries);
+    console.log("Writing " + queries.length + " documents.");
+    queries = [];
+    writeFile("resume_token.json", JSON.stringify(resumeToken), (err) => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+  timeout = setTimeout(() => writeDocuments(collection), 1000);
 }
 
 function encodeCustomer(customer): User {
-    encodedFields.forEach(field => {
-        if (field === 'email') {
-            return customer[field] = encodeEmail(customer[field]);
-        }
-        //Only gets two levels deep into an object since it's enough for the task
-        if (field.indexOf('.') !== -1) {
-            const fieldSplit = field.split('.');
-            return customer[fieldSplit[0]][fieldSplit[1]] =
-                encodeString(customer[fieldSplit[0]][fieldSplit[1]])
-        }
-        customer[field] = encodeString(customer[field]);
-    });
-    return customer;
+  encodedFields.forEach((field) => {
+    if (field === "email") {
+      return (customer[field] = encodeEmail(customer[field]));
+    }
+    //Only gets two levels deep into an object since it's enough for the task
+    if (field.indexOf(".") !== -1) {
+      const fieldSplit = field.split(".");
+      return (customer[fieldSplit[0]][fieldSplit[1]] = encodeString(
+        customer[fieldSplit[0]][fieldSplit[1]]
+      ));
+    }
+    customer[field] = encodeString(customer[field]);
+  });
+  return customer;
 }
 
 ///Deprecated
@@ -109,34 +124,33 @@ function encodeCustomer(customer): User {
 // }
 
 function encodeEmail(email: string) {
-    const emailSplit = email.split('@');
-    return encodeString(emailSplit[0]) + '@' + emailSplit[1];
+  const emailSplit = email.split("@");
+  return encodeString(emailSplit[0]) + "@" + emailSplit[1];
 }
 
 ///It could be more secure but I'll leave it as it is
 ///for the sake of simplicity
 function encodeString(str: string): string {
-    str = createHash('md5').update(str).digest('base64');
-    str = str.slice(0, 8).replace(/\+|\//g, '0');
-    return str;
+  str = createHash("md5").update(str).digest("base64");
+  str = str.slice(0, 8).replace(/\+|\//g, "0");
+  return str;
 }
 
 async function main() {
-    const client = new MongoClient(process.env.DB_URI);
+  const client = new MongoClient(process.env.DB_URI);
 
-    try {
-        await client.connect();
-        const customers = client.db().collection('customers');
-        const customersAnonymised = client.db().collection('customers_anonymised');
-        if (process.argv.length && process.argv.indexOf('--full-reindex') !== -1) {
-            fullSync(customers, customersAnonymised);
-        } else {
-            continuousSync(customers, customersAnonymised);
-        }
+  try {
+    await client.connect();
+    const customers = client.db().collection("customers");
+    const customersAnonymised = client.db().collection("customers_anonymised");
+    if (process.argv.length && process.argv.indexOf("--full-reindex") !== -1) {
+      fullSync(customers, customersAnonymised);
+    } else {
+      continuousSync(customers, customersAnonymised);
     }
-    catch(e) {
-        console.log(e);
-    }
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 main();
